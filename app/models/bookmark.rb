1f583f9d24281ecc3db0e0f24661014982395eb4
Tayson Butler <taycoolbroom@gmail.com>
2021-07-15 12:28:03 +0800

    finished validations and associations

---
+++ app/models/bookmark.rb
@@ -0,0 +1,8 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie, dependent: :destroy
+  belongs_to :list
+
+  validates :movie, uniqueness: { scope: :list }
+  validates :comment, presence: true
+  validates :comment, length: { minimum: 6 }
+end
